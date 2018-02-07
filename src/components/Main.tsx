db041e1570f186aab0b84df82eb5b43726ebc2a0
pnaumov <ecrofeg1@gmail.com>
2018-02-07 18:54:56 +0300

    Some filters

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import Chip from 'material-ui/Chip';
 
 class Main extends React.Component {
 	render() {
-		return <div>
+		return <div className="results-wrapper">
 			<section className="scenarios">
 
 			</section>
 
 			<section className="filters">
-
+				<div className="filters-filter">
+					<Chip label="Авиакомпании" onClick={() => {}}/>
+				</div>
+				<div className="filters-filter">
+					<Chip label="Аэропорты" onClick={() => {}}/>
+				</div>
+				<div className="filters-filter">
+					<Chip label="Время" onClick={() => {}}/>
+				</div>
 			</section>
 
 			<section className="results">
