085cbd4882a3a5f5e499d7b8bd42f5c45dc0c53b
pnaumov <ecrofeg1@gmail.com>
2018-02-13 14:11:10 +0300

    Swipeable views for airports filter

--- src/components/Filters/Airports.tsx
+++ src/components/Filters/Airports.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Tabs, { Tab } from 'material-ui/Tabs';
+import SwipeableViews from 'react-swipeable-views';
 
 import Filter, { State as FilterState, Type as FilterType } from '../Filter';
 import Airport from '../../schemas/Airport';
-
-enum TabType {
-	Departure,
-	Arrival
-}
 
 interface Props {
 	departureAirports: Airport[];
@@ -16,14 +12,14 @@ interface Props {
 }
 
 interface State extends FilterState {
-	activeTab: TabType;
+	activeTab: number;
 }
 
 class Airports extends Filter<Props, State> {
 	state: State = {
 		isOpen: false,
 		element: null,
-		activeTab: TabType.Departure
+		activeTab: 0
 	};
 
 	protected type = FilterType.Airports;
@@ -33,9 +29,16 @@ class Airports extends Filter<Props, State> {
 		super(props);
 
 		this.changeTab = this.changeTab.bind(this);
+		this.changeTabFromSwipe = this.changeTabFromSwipe.bind(this);
 	}
 
-	changeTab(event: React.ChangeEvent<{}>, value: TabType): void {
+	changeTab(event: React.ChangeEvent<{}>, value: number): void {
+		this.setState({
+			activeTab: value
+		} as Partial<State>);
+	}
+
+	changeTabFromSwipe(value: number): void {
 		this.setState({
 			activeTab: value
 		} as Partial<State>);
@@ -45,10 +48,15 @@ class Airports extends Filter<Props, State> {
 		return <div>
 			<AppBar position="static" color="default">
 				<Tabs value={this.state.activeTab} onChange={this.changeTab} indicatorColor="primary" textColor="primary">
-					<Tab label="Туда" value={TabType.Departure}/>
-					<Tab label="Обратно" value={TabType.Arrival}/>
+					<Tab label="Туда" value={0}/>
+					<Tab label="Обратно" value={1}/>
 				</Tabs>
 			</AppBar>
+
+			<SwipeableViews index={this.state.activeTab} onChangeIndex={this.changeTabFromSwipe}>
+				<div>1</div>
+				<div>2</div>
+			</SwipeableViews>
 		</div>;
 	}
 }
