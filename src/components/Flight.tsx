c3ac9410b27c63649dfa7c4820e765b85d9a51d2
pnaumov <ecrofeg1@gmail.com>
2018-02-14 15:13:56 +0300

    Flight block toggle

--- src/components/Flight.tsx
+++ src/components/Flight.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as moment from 'moment';
+import * as classnames from 'classnames';
 import Arrow from 'material-ui-icons/KeyboardArrowDown';
 
 import FlightModel from '../schemas/Flight';
@@ -9,12 +10,32 @@ interface Props {
 	flight: FlightModel;
 }
 
-class Flight extends React.Component<Props> {
-	shouldComponentUpdate(nextProps: Props, nextState: any): boolean {
-		return this.props.flight.id !== nextProps.flight.id;
+interface State {
+	isOpen: boolean;
+}
+
+class Flight extends React.Component<Props, State> {
+	state: State = {
+		isOpen: false
+	};
+
+	constructor(props: Props) {
+		super(props);
+
+		this.toggleDetails = this.toggleDetails.bind(this);
 	}
 
-	render(): React.ReactNode {
+	shouldComponentUpdate(nextProps: Props, nextState: State): boolean {
+		return this.props.flight.id !== nextProps.flight.id || this.state.isOpen !== nextState.isOpen;
+	}
+
+	toggleDetails(): void {
+		this.setState({
+			isOpen: !this.state.isOpen
+		} as State);
+	}
+
+	renderSummary(): React.ReactNode {
 		const flight = this.props.flight;
 		const firstSegment = flight.segments[0];
 		const lastSegment = flight.segments[flight.segments.length - 1];
@@ -24,79 +45,90 @@ class Flight extends React.Component<Props> {
 
 		const totalFlightTimeHuman = moment.duration(totalFlightTime, 'seconds').format('d [д] h [ч] m [мин]');
 
-		return <div className="flight">
-			<div className="flight-summary">
-				<div className="flight-summary__left">
-					<div className="flight-summary-expand">
-						<Arrow/>
+		return <div className={classnames('flight-summary', { 'flight-summary_open': this.state.isOpen })} onClick={this.toggleDetails}>
+			<div className="flight-summary__left">
+				<div className={classnames('flight-summary-expand', { 'flight-summary-expand_open': this.state.isOpen })}>
+					<Arrow/>
+				</div>
+
+				<div className="flight-summary-logo">
+					<img className="flight-summary-logo__image" src={`http://nemo1${firstSegment.airline.logoIcon}`}/>
+				</div>
+
+				<div className="flight-summary-stage">
+					<div className="flight-summary-stage__time">
+						{firstSegment.depDate.format('HH:MM')}
 					</div>
 
-					<div className="flight-summary-logo">
-						<img className="flight-summary-logo__image" src={`http://nemo1${firstSegment.airline.logoIcon}`}/>
-					</div>
-
-					<div className="flight-summary-stage">
-						<div className="flight-summary-stage__time">
-							{firstSegment.depDate.format('HH:MM')}
-						</div>
-
-						<div className="flight-summary-stage__location">
-							{firstSegment.depAirport.IATA}
-						</div>
-					</div>
-
-					<div className="flight-summary-stage-routeInfo">
-						<div className="flight-summary-stage-routeInfo__arrow"/>
-						<span className="flight-summary-stage-routeInfo__flightTime">{totalFlightTimeHuman}</span>
-					</div>
-
-					<div className="flight-summary-stage">
-						<div className="flight-summary-stage__time">
-							{lastSegment.arrDate.format('HH:MM')}
-						</div>
-
-						<div className="flight-summary-stage__location">
-							{lastSegment.arrAirport.IATA}
-						</div>
+					<div className="flight-summary-stage__location">
+						{firstSegment.depAirport.IATA}
 					</div>
 				</div>
 
-				<div className="flight-summary__middle">
-					<div className="flight-summary-transfers">
-						{isDirect ? 'прямой' : null}
-						{!isDirect ? flight.segments.slice(0, flight.segments.length - 1).map((segment, index) => {
-							const waitingTime = moment.duration(segment.waitingTime, 'seconds').format('d [д] h [ч] m [мин]');
-
-							return <div className="flight-summary-transfers__item" key={index}>{waitingTime} пересадка в
-								городе {segment.arrAirport.city.name}</div>;
-						}) : null}
-					</div>
-
-					<div className="flight-summary-route">
-						{firstSegment.depAirport.name} &mdash; {lastSegment.arrAirport.name}
-					</div>
+				<div className="flight-summary-stage-routeInfo">
+					<div className="flight-summary-stage-routeInfo__arrow"/>
+					<span className="flight-summary-stage-routeInfo__flightTime">{totalFlightTimeHuman}</span>
 				</div>
 
-				<div className="flight-summary__right">
-					<div className="flight-summary-price">
-						<div className="flight-summary-price__amount">
-							{!isOW ? 'от' : null}
-							<span className="flight-summary-price__amount-wrapper">
-								{flight.totalPrice.amount}
-								<span className="flight-summary-price__amount-wrapper__currency">{flight.totalPrice.currency}</span>
-							</span>
-						</div>
-
-						{!isOW ? <div className="flight-summary-price__scope">
-							туда и обратно
-						</div> : null}
+				<div className="flight-summary-stage">
+					<div className="flight-summary-stage__time">
+						{lastSegment.arrDate.format('HH:MM')}
 					</div>
 
-					<div className="flight-summary-buy">
-						Выбрать
+					<div className="flight-summary-stage__location">
+						{lastSegment.arrAirport.IATA}
 					</div>
 				</div>
 			</div>
+
+			<div className="flight-summary__middle">
+				<div className="flight-summary-transfers">
+					{isDirect ? 'прямой' : null}
+					{!isDirect ? flight.segments.slice(0, flight.segments.length - 1).map((segment, index) => {
+						const waitingTime = moment.duration(segment.waitingTime, 'seconds').format('d [д] h [ч] m [мин]');
+
+						return <div className="flight-summary-transfers__item" key={index}>{waitingTime} пересадка в
+							городе {segment.arrAirport.city.name}</div>;
+					}) : null}
+				</div>
+
+				<div className="flight-summary-route">
+					{firstSegment.depAirport.name} &mdash; {lastSegment.arrAirport.name}
+				</div>
+			</div>
+
+			<div className="flight-summary__right">
+				<div className="flight-summary-price">
+					<div className="flight-summary-price__amount">
+						{!isOW ? 'от' : null}
+						<span className="flight-summary-price__amount-wrapper">
+							{flight.totalPrice.amount}
+							<span className="flight-summary-price__amount-wrapper__currency">{flight.totalPrice.currency}</span>
+						</span>
+					</div>
+
+					{!isOW ? <div className="flight-summary-price__scope">
+						туда и обратно
+					</div> : null}
+				</div>
+
+				<div className="flight-summary-buy">
+					Выбрать
+				</div>
+			</div>
+		</div>;
+	}
+
+	renderDetails(): React.ReactNode {
+		return <div className={classnames('flight-details', { 'flight-details_open': this.state.isOpen })}>
+			123
+		</div>;
+	}
+
+	render(): React.ReactNode {
+		return <div className={classnames('flight', { flight_open: this.state.isOpen })}>
+			{this.renderSummary()}
+			{this.renderDetails()}
 		</div>;
 	}
 }
