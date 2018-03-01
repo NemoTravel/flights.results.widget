25522eefa49bd84109d3f06f994c3cb6bbd41729
pnaumov <ecrofeg1@gmail.com>
2018-03-01 14:14:09 +0300

    Sort arrival date by UNIX timestamp

--- src/store/sorting/selectors.ts
+++ src/store/sorting/selectors.ts
@@ -2,6 +2,16 @@ import { ApplicationState, SortingDirection, SortingState } from '../../state';
 import Flight from '../../schemas/Flight';
 import SegmentModel from '../../schemas/Segment';
 
+const MINUTES_IN_HOUR = 60;
+
+/**
+ * Compare two objects considering the sorting direction.
+ *
+ * @param a
+ * @param b
+ * @param {SortingDirection} direction
+ * @returns {number}
+ */
 const compare = (a: any, b: any, direction: SortingDirection): number => {
 	if (a > b) {
 		return direction === SortingDirection.ASC ? 1 : -1;
@@ -14,18 +24,42 @@ const compare = (a: any, b: any, direction: SortingDirection): number => {
 	}
 };
 
-const MINUTES_IN_HOUR = 60;
-
+/**
+ * @param {Flight} flight
+ * @returns {number}
+ */
 export const getTotalFlightTime = (flight: Flight): number => {
 	return flight.segments.reduce((result: number, segment: SegmentModel) => result + segment.flightTime + segment.waitingTime, 0);
 };
 
+/**
+ * Current active sorting.
+ *
+ * @param {ApplicationState} state
+ * @returns {SortingState}
+ */
 export const getCurrentSorting = (state: ApplicationState): SortingState => state.sorting;
 
+/**
+ * Compare two flights by total price.
+ *
+ * @param {Flight} a
+ * @param {Flight} b
+ * @param {SortingDirection} direction
+ * @returns {number}
+ */
 export const priceCompareFunction = (a: Flight, b: Flight, direction: SortingDirection): number => {
 	return compare(a.totalPrice.amount, b.totalPrice.amount, direction);
 };
 
+/**
+ * Compare two flights by departure time.
+ *
+ * @param {Flight} a
+ * @param {Flight} b
+ * @param {SortingDirection} direction
+ * @returns {number}
+ */
 export const departureTimeCompareFunction = (a: Flight, b: Flight, direction: SortingDirection): number => {
 	const aTime = a.segments[0].depDate.hours() * MINUTES_IN_HOUR + a.segments[0].depDate.minutes();
 	const bTime = b.segments[0].depDate.hours() * MINUTES_IN_HOUR + b.segments[0].depDate.minutes();
@@ -33,13 +67,29 @@ export const departureTimeCompareFunction = (a: Flight, b: Flight, direction: So
 	return compare(aTime, bTime, direction);
 };
 
+/**
+ * Compare two flights by arrival time.
+ *
+ * @param {Flight} a
+ * @param {Flight} b
+ * @param {SortingDirection} direction
+ * @returns {number}
+ */
 export const arrivalTimeCompareFunction = (a: Flight, b: Flight, direction: SortingDirection): number => {
-	const aTime = a.segments[a.segments.length - 1].arrDate.hours() * MINUTES_IN_HOUR + a.segments[a.segments.length - 1].arrDate.minutes();
-	const bTime = b.segments[b.segments.length - 1].arrDate.hours() * MINUTES_IN_HOUR + b.segments[b.segments.length - 1].arrDate.minutes();
+	const aTime = a.segments[a.segments.length - 1].arrDate.unix();
+	const bTime = b.segments[b.segments.length - 1].arrDate.unix();
 
 	return compare(aTime, bTime, direction);
 };
 
+/**
+ * Compare two flights by total flight time.
+ *
+ * @param {Flight} a
+ * @param {Flight} b
+ * @param {SortingDirection} direction
+ * @returns {number}
+ */
 export const flightTimeCompareFunction = (a: Flight, b: Flight, direction: SortingDirection): number => {
 	const aTime = getTotalFlightTime(a);
 	const bTime = getTotalFlightTime(b);
