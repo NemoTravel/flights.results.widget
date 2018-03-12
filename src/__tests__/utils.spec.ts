d7f26bb12033116a7f060126c6fcde143f02bae3
pnaumov <ecrofeg1@gmail.com>
2018-03-12 23:50:40 +0400

    Tests

---
+++ src/__tests__/utils.spec.ts
@@ -0,0 +1,45 @@
+/* global describe */
+/* global it */
+/* global expect */
+import { addCodeInList, declension, removeCodeFromList } from '../utils';
+
+describe('utils', () => {
+	describe('addCodeInList', () => {
+		it('should add new string to an array', () => {
+			const list: string[] = ['code_1', 'code_2'];
+			const newList = addCodeInList(list, 'code_3');
+
+			expect(!!newList.find(value => value === 'code_3')).toEqual(true);
+		});
+
+		it('should not add duplicate string to an array', () => {
+			const list: string[] = ['code_1', 'code_2'];
+			const newList = addCodeInList(list, 'code_2');
+
+			expect(newList.length).toEqual(list.length);
+		});
+	});
+
+	describe('removeCodeFromList', () => {
+		it('should remove string from an array', () => {
+			const list: string[] = ['code_1', 'code_2'];
+			const newList = removeCodeFromList(list, 'code_2');
+
+			expect(newList.length).toEqual(list.length - 1);
+		});
+	});
+
+	describe('declension', () => {
+		it('should return `Москва`', () => {
+			expect(declension('Москва')).toEqual('Москве');
+		});
+
+		it('should return `Казани`', () => {
+			expect(declension('Казань')).toEqual('Казани');
+		});
+
+		it('should return `Клинцах`', () => {
+			expect(declension('Клинцы')).toEqual('Клинцах');
+		});
+	});
+});
