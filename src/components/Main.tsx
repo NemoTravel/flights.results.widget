3db73ef4cda650fddbcaa4b817d7a71dba163ecc
ecrofeg <ecrofeg1@gmail.com>
2018-04-12 18:03:30 +0400

    Import Search Form as a component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 import { connect } from 'react-redux';
 import { CircularProgress } from 'material-ui/Progress';
+import { Component } from '@nemo.travel/search-widget';
 
 import Results from './Results';
 import AlternativeFlights from './AlternativeFlights';
 import { ApplicationState } from '../state';
 import { isSelectionComplete } from '../store/selectedFlights/selectors';
 import Toolbar from './Toolbar';
+import { REQUEST_URL } from '../utils';
 
 interface StateProps {
 	isLoading: boolean;
@@ -17,6 +19,8 @@ interface StateProps {
 class Main extends React.Component<StateProps> {
 	render(): React.ReactNode {
 		return <div className={classNames('results', { results_isLoading: this.props.isLoading })}>
+			<Component nemoURL={REQUEST_URL}/>
+
 			<div className="results-loader">
 				<CircularProgress color="secondary" variant="indeterminate"/>
 			</div>
