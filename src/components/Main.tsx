9d4535511faabfd38f087ff6051d02e1b566db07
ecrofeg <ecrofeg1@gmail.com>
2018-04-12 18:46:08 +0400

    Import Search Form as a component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 import { connect } from 'react-redux';
 import { CircularProgress } from 'material-ui/Progress';
-import { Component } from '@nemo.travel/search-widget';
+import { Component as SearchForm } from '@nemo.travel/search-widget';
 
 import Results from './Results';
 import AlternativeFlights from './AlternativeFlights';
-import { ApplicationState } from '../state';
+import { ApplicationState, Language } from '../state';
 import { isSelectionComplete } from '../store/selectedFlights/selectors';
 import Toolbar from './Toolbar';
 import { REQUEST_URL } from '../utils';
@@ -18,19 +18,23 @@ interface StateProps {
 
 class Main extends React.Component<StateProps> {
 	render(): React.ReactNode {
-		return <div className={classNames('results', { results_isLoading: this.props.isLoading })}>
-			<Component nemoURL={REQUEST_URL}/>
+		return (
+			<div className={classNames('results', { results_isLoading: this.props.isLoading })}>
+				<SearchForm nemoURL={REQUEST_URL} locale={Language.Russian}/>
 
-			<div className="results-loader">
-				<CircularProgress color="secondary" variant="indeterminate"/>
+				<div className="results__inner">
+					<div className="results-loader">
+						<CircularProgress color="secondary" variant="indeterminate"/>
+					</div>
+
+					{!this.props.isLoading ? (
+						this.props.isSelectionComplete ? <AlternativeFlights/> : <Results/>
+					) : null}
+
+					<Toolbar/>
+				</div>
 			</div>
-
-			{!this.props.isLoading ? (
-				this.props.isSelectionComplete ? <AlternativeFlights/> : <Results/>
-			) : null}
-
-			<Toolbar/>
-		</div>;
+		);
 	}
 }
 
