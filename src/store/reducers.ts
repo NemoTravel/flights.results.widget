de45b5372b9ae79509191cc0d185ca2f68955ee3
ecrofeg <ecrofeg1@gmail.com>
2018-05-07 17:21:07 +0400

    Shorthands for reducer

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,30 +1,30 @@
 import { combineReducers } from 'redux';
 
 import { ApplicationState } from '../state';
-import { flightsByLegsReducer } from './flightsByLegs/reducers';
-import { loadingReducer } from './isLoading/reducers';
-import { configReducer } from './config/reducers';
-import { filtersReducer } from './filters/reducers';
-import { currentLegReducer } from './currentLeg/reducers';
-import { selectedFlightsReducer } from './selectedFlights/reducers';
-import { flightsReducer } from './flights/reducers';
-import { alternativeFlightsReducer } from './alternativeFlights/reducers';
-import { sortingReducer } from './sorting/reducers';
-import { flightsRTReducer } from './flightsRT/reducers';
-import { legsReducer } from './legs/reducers';
-import { showAllFlightsReducer } from './showAllFlights/reducers';
+import { flightsByLegsReducer as flightsByLegs } from './flightsByLegs/reducers';
+import { loadingReducer as isLoading } from './isLoading/reducers';
+import { configReducer as config } from './config/reducers';
+import { filtersReducer as filters } from './filters/reducers';
+import { currentLegReducer as currentLeg } from './currentLeg/reducers';
+import { selectedFlightsReducer as selectedFlights } from './selectedFlights/reducers';
+import { flightsReducer as flights } from './flights/reducers';
+import { alternativeFlightsReducer as alternativeFlights } from './alternativeFlights/reducers';
+import { sortingReducer as sorting } from './sorting/reducers';
+import { flightsRTReducer as flightsRT } from './flightsRT/reducers';
+import { legsReducer as legs } from './legs/reducers';
+import { showAllFlightsReducer as showAllFlights } from './showAllFlights/reducers';
 
 export const rootReducer = combineReducers<ApplicationState>({
-	isLoading: loadingReducer,
-	flights: flightsReducer,
-	flightsRT: flightsRTReducer,
-	flightsByLegs: flightsByLegsReducer,
-	filters: filtersReducer,
-	config: configReducer,
-	currentLeg: currentLegReducer,
-	selectedFlights: selectedFlightsReducer,
-	alternativeFlights: alternativeFlightsReducer,
-	sorting: sortingReducer,
-	legs: legsReducer,
-	showAllFlights: showAllFlightsReducer
+	isLoading,
+	flights,
+	flightsRT,
+	flightsByLegs,
+	filters,
+	config,
+	currentLeg,
+	selectedFlights,
+	alternativeFlights,
+	sorting,
+	legs,
+	showAllFlights
 });
