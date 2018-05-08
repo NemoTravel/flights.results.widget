2823005d7ebc25aaf33ecad542ec30e44580b5c1
ecrofeg <ecrofeg1@gmail.com>
2018-05-08 17:03:38 +0400

    Split sagas into files

---
+++ src/store/sagas/searchFareFamilies.ts
@@ -0,0 +1,62 @@
+import { SEARCH_FARE_FAMILIES } from '../actions';
+import { call, fork, put, select, takeEvery } from 'redux-saga/effects';
+import { startLoading, stopLoading } from '../isLoading/actions';
+import FareFamiliesCombinations from '../../schemas/FareFamiliesCombinations';
+import { setSelectedFamily } from '../fareFamilies/selectedFamilies/actions';
+import loadFareFamilies from '../../services/requests/fareFamilies';
+import { setCombinations } from '../fareFamilies/fareFamiliesCombinations/actions';
+import { ApplicationState } from '../../state';
+
+function* runFareFamiliesSearch(flightId: number, legId: number) {
+	// Get fare families combinations for given flight.
+	const results: FareFamiliesCombinations = yield call(loadFareFamilies, flightId);
+
+	// Put them in Store.
+	yield put(setCombinations(legId, results));
+
+	// Split initial combination key apart.
+	const combinationParts = results ? results.initialCombination.split('_') : [];
+	const numOfSegments = combinationParts.length;
+
+	// Pre-select fare families.
+	for (let segmentId = 0; segmentId < numOfSegments; segmentId++) {
+		const familyId = combinationParts[segmentId];
+
+		yield put(setSelectedFamily(legId, segmentId, familyId));
+	}
+}
+
+function* searchFareFamilies(flightIds: number[]) {
+	const numOfLegs = flightIds.length;
+
+	// Run fare families search for each given flight ID.
+	for (let i = 0; i < numOfLegs; i++) {
+		yield fork(runFareFamiliesSearch, flightIds[i], i);
+	}
+}
+
+function* worker() {
+	// Launch loading animation.
+	yield put(startLoading());
+
+	const state: ApplicationState = yield select();
+	const selectedFlights = state.selectedFlights;
+	const flightIds: number[] = [];
+
+	// Collect IDs of selected flights.
+	for (const legId in selectedFlights) {
+		if (selectedFlights.hasOwnProperty(legId)) {
+			flightIds.push(selectedFlights[legId]);
+		}
+	}
+
+	// Run all searches.
+	yield call(searchFareFamilies, flightIds);
+
+	// Stop loading animation.
+	yield put(stopLoading());
+}
+
+export default function* searchFareFamiliesSaga() {
+	yield takeEvery(SEARCH_FARE_FAMILIES, worker);
+}
---
+++ src/store/sagas/startSearch.ts
@@ -0,0 +1,67 @@
+import Flight from '../../models/Flight';
+import { call, fork, put, takeEvery } from 'redux-saga/effects';
+import RequestInfo from '../../schemas/RequestInfo';
+import { startLoading, stopLoading } from '../isLoading/actions';
+import { addFlightsRT } from '../flightsRT/actions';
+import loadSearchResults from '../../services/requests/results';
+import { setLegs } from '../legs/actions';
+import { SearchAction, SearchActionPayload, START_SEARCH } from '../actions';
+import { addFlights } from '../flights/actions';
+import { setFlightsByLeg } from '../flightsByLegs/actions';
+import Leg from '../../schemas/Leg';
+
+const createLegs = (requests: RequestInfo[]): Leg[] => {
+	return requests.map((requestInfo, index) => {
+		return {
+			id: index,
+			departure: requestInfo.segments[0].departure,
+			arrival: requestInfo.segments[0].arrival,
+			date: requestInfo.segments[0].departureDate
+		};
+	});
+};
+
+function* runSearch(request: RequestInfo, index: number) {
+	const flights: Flight[] = yield call(loadSearchResults, request);
+
+	yield put(addFlights(flights));
+	yield put(setFlightsByLeg(flights, index));
+}
+
+function* runRTSearch(request: RequestInfo) {
+	const RTresults: Flight[] = yield call(loadSearchResults, request);
+
+	yield put(addFlightsRT(RTresults));
+}
+
+function* runSearches(data: SearchActionPayload) {
+	if (data.RTRequest) {
+		// Run async round-trip search.
+		yield fork(runRTSearch, data.RTRequest);
+	}
+
+	// Split round-trip search into separate one-way searches.
+	const numOfLegs = data.requests.length;
+
+	for (let i = 0; i < numOfLegs; i++) {
+		yield fork(runSearch, data.requests[i], i);
+	}
+}
+
+function* worker({ payload }: SearchAction) {
+	// Launch loading animation.
+	yield put(startLoading());
+
+	// Create legs array.
+	yield put(setLegs(createLegs(payload.requests)));
+
+	// Run all searches.
+	yield call(runSearches, payload);
+
+	// Stop loading animation.
+	yield put(stopLoading());
+}
+
+export default function* startSearchSaga() {
+	yield takeEvery(START_SEARCH, worker);
+}
