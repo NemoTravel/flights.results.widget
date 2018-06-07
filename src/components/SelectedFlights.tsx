268bb1e76759ab35c978eb8b7dae0fcc87e99c87
ecrofeg <ecrofeg1@gmail.com>
2018-06-07 15:39:44 +0400

    Remove selected flights placeholders

--- src/components/SelectedFlights.tsx
+++ src/components/SelectedFlights.tsx
@@ -1,23 +1,16 @@
 import * as React from 'react';
-import * as classnames from 'classnames';
 import autobind from 'autobind-decorator';
-import FlightTakeOffIcon from '@material-ui/icons/FlightTakeoff';
 
 import FlightModel from '../models/Flight';
-import Leg from '../schemas/Leg';
-import { getLegs, isRT } from '../store/legs/selectors';
 import { connect } from '../utils';
-import { getCurrentLeg } from '../store/currentLeg/selectors';
 import { getSelectedFlights } from '../store/selectedFlights/selectors';
 import SelectedFlight from './SelectedFlight';
 import { goToLeg } from '../store/currentLeg/actions';
 import { FlightsReplacement } from '../schemas/SelectedFlight';
 import { getPricesForSelectedFlights } from '../store/selectors';
+import Typography from '@material-ui/core/Typography';
 
 interface StateProps {
-	currentLeg: Leg;
-	legs: Leg[];
-	isRT: boolean;
 	selectedFlights: FlightModel[];
 	prices: FlightsReplacement;
 }
@@ -29,7 +22,8 @@ interface DispatchProps {
 type Props = StateProps & DispatchProps;
 
 class SelectedFlights extends React.Component<Props> {
-	renderFlightBlock(flight: FlightModel): React.ReactNode {
+	@autobind
+	renderFlight(flight: FlightModel): React.ReactNode {
 		const { goToLeg, prices } = this.props;
 
 		return (
@@ -44,45 +38,24 @@ class SelectedFlights extends React.Component<Props> {
 		);
 	}
 
-	renderPlaceholder(leg: Leg): React.ReactNode {
-		const { currentLeg, isRT } = this.props;
-
-		return (
-			<div key={leg.id} className={classnames('results-selectedFlights-leg', { 'results-selectedFlights-leg_active': currentLeg.id === leg.id })}>
-				<div className={classnames('results-selectedFlights-leg__icon', { 'results-selectedFlights-leg__icon_reverse': isRT && leg.id === 1 })}>
-					<FlightTakeOffIcon/>
-				</div>
-
-				{leg.departure.city.name} &mdash; {leg.arrival.city.name}
-			</div>
-		);
-	}
-
-	@autobind
-	renderLegContent(leg: Leg): React.ReactNode {
-		const { selectedFlights } = this.props;
-
-		if (selectedFlights.hasOwnProperty(leg.id)) {
-			return this.renderFlightBlock(selectedFlights[leg.id]);
-		}
-		else {
-			return this.renderPlaceholder(leg);
-		}
-	}
-
 	render(): React.ReactNode {
 		return (
 			<div className="results-selectedFlights">
-				{this.props.legs.map(this.renderLegContent)}
+				{this.props.selectedFlights.length ? (
+					<div className="results-selectedFlights-title">
+						<Typography variant="headline">Выбранные рейсы</Typography>
+					</div>
+				) : null}
+
+				<div className="results-selectedFlights__wrapper">
+					{this.props.selectedFlights.map(this.renderFlight)}
+				</div>
 			</div>
 		);
 	}
 }
 
 export default connect<StateProps, DispatchProps>({
-	isRT: isRT,
-	currentLeg: getCurrentLeg,
 	selectedFlights: getSelectedFlights,
-	legs: getLegs,
 	prices: getPricesForSelectedFlights
 }, { goToLeg })(SelectedFlights);
