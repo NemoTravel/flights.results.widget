8b84fe49460acb2c351504eb5c0498acba2a7919
akizimov <a.kizimov@mute-lab.com>
2018-07-05 16:29:00 +0300

    37631: hide search form on tablet

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -9,6 +9,7 @@ import { RootState } from '../store/reducers';
 import { getLocale, getNemoURL } from '../store/config/selectors';
 import { startSearch } from '../store/actions';
 import { loadSearchResults } from '../store/results/actions';
+import { i18n } from '../i18n';
 
 type StateProps = ComponentProps & Partial<RouterState>;
 
@@ -19,12 +20,48 @@ interface DispatchProps {
 
 type Props = StateProps & DispatchProps;
 
-class SearchForm extends React.Component<Props> {
+interface State {
+	openedOnMobile: boolean;
+}
+
+class SearchForm extends React.Component<Props, State> {
+	state: State = {
+		openedOnMobile: false
+	};
+
 	protected searchForm: SearchFormComponent = null;
+	protected searchFormWrapper: HTMLElement = null;
 
 	@autobind
 	getSearchFormRef(component: SearchFormComponent): void {
 		this.searchForm = component;
+	}
+
+	@autobind
+	getSearchFormWrapperRef(ref: HTMLElement): void {
+		this.searchFormWrapper = ref;
+	}
+
+	componentWillMount(): void {
+		window.addEventListener('click', this.handleOutsideClick);
+	}
+
+	componentWillUnmount(): void {
+		window.removeEventListener('click', this.handleOutsideClick);
+	}
+
+	@autobind
+	handleOutsideClick(): void {
+		this.setState({ openedOnMobile: false });
+	}
+
+	@autobind
+	shortOpen(): void {
+		this.setState({ openedOnMobile: true });
+	}
+
+	wrapperClick(event: React.MouseEvent<HTMLElement>): void {
+		event.stopPropagation();
 	}
 
 	componentDidMount(): void {
@@ -39,14 +76,21 @@ class SearchForm extends React.Component<Props> {
 	render(): React.ReactNode {
 		const isResultsPage = this.props.location.pathname !== '/';
 		const classNames = classnames('results-searchForm', { 'results-searchForm_pinned': isResultsPage });
+		const wrapperClassNames = classnames('results-searchForm__wrapper', { 'results-searchForm__wrapper_opened': this.state.openedOnMobile });
 
 		return <div className={classNames}>
-			<SearchFormComponent
-				ref={this.getSearchFormRef}
-				nemoURL={this.props.nemoURL}
-				locale={this.props.locale}
-				onSearch={this.props.startSearch}
-			/>
+			<div className={wrapperClassNames} ref={this.getSearchFormWrapperRef} onClick={this.wrapperClick}>
+				<SearchFormComponent
+					ref={this.getSearchFormRef}
+					nemoURL={this.props.nemoURL}
+					locale={this.props.locale}
+					onSearch={this.props.startSearch}
+				/>
+
+				<div className="results-searchForm-short" onClick={this.shortOpen}>
+					<div className="results-searchForm-short__button">{i18n('results-searchForm-change')}</div>
+				</div>
+			</div>
 		</div>;
 	}
 }
